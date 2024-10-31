fd4032b0aded79cb4c95be2516951b4f53fcc5dc
elija feigl <elija-feigl@users.noreply.github.com>
2024-10-31 10:44:03 +0100

    ci: fix folder paths in legacy test

--- analysis/tests/test.sh
+++ analysis/tests/test.sh
@@ -28,51 +28,51 @@ test() {
 }
 
 echo -n "Testing align..."
-test "../src/oxDNA_analysis_tools/align.py minitraj.dat aligned.dat"
+test "../src/oxDNA_analysis_tools/align.py resources/minitraj.dat resources/aligned.dat"
 
 echo -n "Testing backbone_flexibility..."
-test "../src/oxDNA_analysis_tools/backbone_flexibility.py rna_tile.top minitraj.dat"
+test "../src/oxDNA_analysis_tools/backbone_flexibility.py resources/rna_tile.top resources/minitraj.dat"
 
 echo -n "Testing bond_analysis..."
-test "../src/oxDNA_analysis_tools/bond_analysis.py -p2 input_rna minitraj.dat pairs.txt"
+test "../src/oxDNA_analysis_tools/bond_analysis.py -p2 resources/input_rna resources/minitraj.dat resources/pairs.txt"
 
 echo -n "Testing mean and deviations..."
-test "../src/oxDNA_analysis_tools/mean.py -p 2 -d devs.json minitraj.dat"
+test "../src/oxDNA_analysis_tools/mean.py -p 2 -d devs.json resources/minitraj.dat"
 
 echo -n "Testing centroid with indexing..."
-test "../src/oxDNA_analysis_tools/centroid.py -i index.txt mean.dat minitraj.dat"
+test "../src/oxDNA_analysis_tools/centroid.py -i resources/index.txt mean.dat resources/minitraj.dat"
 
 echo -n "Testing contact_map..."
-test "../src/oxDNA_analysis_tools/contact_map.py minitraj.dat"
+test "../src/oxDNA_analysis_tools/contact_map.py resources/minitraj.dat"
 
 echo -n "Testing distance and clustering (this one takes a while because of the plot)..."
-test "../src/oxDNA_analysis_tools/distance.py -c -i minitraj.dat 1 3 5 67 34 56"
+test "../src/oxDNA_analysis_tools/distance.py -c -i resources/minitraj.dat 1 3 5 67 34 56"
 
 echo -n "Testing duplex_finder..."
-test "../src/oxDNA_analysis_tools/duplex_finder.py input_rna minitraj.dat"
+test "../src/oxDNA_analysis_tools/duplex_finder.py resources/input_rna resources/minitraj.dat"
 
 echo -n "Testing duplex_angle_plotter..."
 test "../src/oxDNA_analysis_tools/duplex_angle_plotter.py -o angle.png -i angles.txt 7 37"
 
 echo -n "Testing generate_force..."
-test "../src/oxDNA_analysis_tools/generate_force.py -f gen_pairs.txt input_rna minitraj.dat"
+test "../src/oxDNA_analysis_tools/generate_force.py -f gen_pairs.txt resources/input_rna resources/minitraj.dat"
 
 echo -n "Testing minify..."
-test "../src/oxDNA_analysis_tools/minify.py -a -d2 minitraj.dat min.dat"
+test "../src/oxDNA_analysis_tools/minify.py -a -d2 resources/minitraj.dat min.dat"
 
 echo -n "Testing multidimensional_scaling_mean..."
-test "../src/oxDNA_analysis_tools/multidimensional_scaling_mean.py minitraj.dat -o meanM -d devsM"
+test "../src/oxDNA_analysis_tools/multidimensional_scaling_mean.py resources/minitraj.dat -o meanM -d devsM"
 
 echo -n "Testing output_bonds..."
-test "../src/oxDNA_analysis_tools/output_bonds.py -v energy.json input_rna minitraj.dat"
+test "../src/oxDNA_analysis_tools/output_bonds.py -v energy.json resources/input_rna resources/minitraj.dat"
 
 echo -n "Testing pca"
-test "../src/oxDNA_analysis_tools/pca.py minitraj.dat mean.dat pca.json"
+test "../src/oxDNA_analysis_tools/pca.py resources/minitraj.dat mean.dat pca.json"
 
 echo -n "Testing subset_trajectory..."
-test "../src/oxDNA_analysis_tools/subset_trajectory.py minitraj.dat rna_tile.top -i index.txt sub.dat"
+test "../src/oxDNA_analysis_tools/subset_trajectory.py resources/minitraj.dat resources/rna_tile.top -i resources/index.txt sub.dat"
 
 echo -n "Testing superimpose..."
-test "../src/oxDNA_analysis_tools/superimpose.py mean.dat centroid.dat"
+test "../src/oxDNA_analysis_tools/superimpose.py mean.dat entroid.dat"
 
 echo ""
--- analysis/tests/test_cli/test_cli.py
+++ analysis/tests/test_cli/test_cli.py
@@ -1,12 +1,12 @@
-import time
+import logging
 from pathlib import Path
 
 from oxDNA_analysis_tools.align import align
-from oxDNA_analysis_tools.UTILS.logger import log
 
 
-def test_align(align_input):
-    start_time = time.time()
+def test_align(align_input, caplog):
+    caplog.set_level(logging.DEBUG, logger="oxDNA_analysis_tools")
+
     align(
         traj=align_input["traj"],
         outfile=align_input["outfile"],
@@ -15,9 +15,14 @@ def test_align(align_input):
         ref_conf=align_input["ref_conf"],
         center=align_input["center"],
     )
-    log("--- %s seconds ---" % (time.time() - start_time))
+
+    n_criticals = 0
+    for record in caplog.records:
+        if record.levelname == "CRITICAL":
+            n_criticals += 1
 
     outfile = Path(align_input["outfile"])
+    assert n_criticals == 0
     assert outfile.exists()
     assert outfile.is_file()
     assert outfile.stat().st_size > 0
